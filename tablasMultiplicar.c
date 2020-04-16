318a44ac907d283ffc31464160c99fcf5052ce64
Victor Bayas <49411169+victor-bayas@users.noreply.github.com>
2020-04-16 16:49:41 -0500

    change logic

--- tablasMultiplicar.c
+++ tablasMultiplicar.c
@@ -2,10 +2,10 @@
 void main()
 {
   int num,cant,cont,resultado;
-  printf("Ingrese el limite: ");
-  scanf("%i",&cant);
   printf("Ingrese el numero: ");
   scanf("%i",&num);
+  printf("Ingrese el limite: ");
+  scanf("%i",&cant);
   for (cont=1; cont<=cant; cont++)
   {
     resultado=num*cont;
